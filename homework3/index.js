a980758a91122aa5d8d86ff8c93a6eebc5ba1784
Orhan <ya.eskimo-callboy@yandex.by>
2021-10-07 12:02:48 +0300

    add another function

--- homework3/index.js
+++ homework3/index.js
@@ -1,7 +1,7 @@
 Array.prototype.myFilter = function (callback, thisArg) {
   if (typeof callback !== "function") {
     throw new Error();
-  }
+  };
 
   const context = thisArg || this;
   const newArray = [];
@@ -9,8 +9,16 @@ Array.prototype.myFilter = function (callback, thisArg) {
   this.forEach((item, i, arr) => {
     if (callback.call(context, item, i, context)) {
       newArray.push(item);
-    }
+    };
   });
 
   return newArray;
+};
+
+function createDebounceFunction(callback, timeout) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { callback.apply(this, args); }, timeout);
+  };
 };
