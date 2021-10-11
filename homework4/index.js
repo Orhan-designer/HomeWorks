5feaccf390b7a0b9dac67c548f0e5a51bb4e235c
Orhan <ya.eskimo-callboy@yandex.by>
2021-10-11 13:21:48 +0300

    changes in function concatStrings

--- homework4/index.js
+++ homework4/index.js
@@ -1,22 +1,18 @@
 function concatStrings(str, separator) {
-  let sep;
-  let newString;
-  typeof separator === 'string' ? (sep = separator) : (sep = '');
-  typeof str === 'string' ? (newString = str) : (newString = '');
-
-  function concat(string) {
-    if (typeof string === 'string') {
-      newString += sep + string;
+    let sep;
+    let newString;
+    typeof separator === 'string' ? (sep = separator) : (sep = '');
+    typeof str === 'string' ? (newString = str) : (newString = '');
+  
+    return function concat(string) {
+        if(string && typeof string === 'string'){
+            newString += sep + string;
+            return concat;
+        } else {
+            return newString;
+        };
     };
-    return concat;
   };
-
-  concat.toString = () => {
-    return newString;
-  };
-
-  return concat;
-};
 
 class Calculator {
   constructor(valueX, valueY) {
@@ -24,43 +20,43 @@ class Calculator {
     this.valueY = valueY;
     if (!Number.isInteger(valueX) || !Number.isInteger(valueY)) {
       throw new Error();
-    };
+    }
 
-    if (!typeof BigInt(1) === 'bigint') {
+    if (!typeof BigInt(1) === "bigint") {
       throw new Error();
-    };
-  };
+    }
+  }
 
   setX(num) {
     if (!Number.isInteger(num)) {
       throw new Error();
-    };
-    return this.valueX = num;
-  };
+    }
+    return (this.valueX = num);
+  }
 
   setY(num) {
     if (!Number.isInteger(num)) {
       throw new Error();
-    };
-    return this.valueY = num;
-  };
+    }
+    return (this.valueY = num);
+  }
 
   logSum() {
     console.log(this.valueX + this.valueY);
-  };
+  }
 
   logSub() {
     console.log(this.valueX - this.valueY);
-  };
+  }
 
   logMul() {
     console.log(this.valueX * this.valueY);
-  };
+  }
 
   logDiv() {
     if (this.valueY === 0) {
       throw new Error();
-    };
+    }
     console.log(this.valueX / this.valueY);
-  };
-};
+  }
+}
